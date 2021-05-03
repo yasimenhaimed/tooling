322ceca50c8d97395903340c24db6766fffbd9c3
Anefu <45211871+Anefu@users.noreply.github.com>
2021-05-03 11:44:50 +0100

    Update functions.php

--- html/functions.php
+++ html/functions.php
@@ -2,7 +2,7 @@
 session_start();
 
 // connect to database
-$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');
+$db = mysqli_connect('172.31.53.80', 'webaccess', 'admin', 'tooling');
 
 // Check connection
 // if (mysqli_connect_errno()) {
